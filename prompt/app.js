924409e3d1a4137bace30634fc04232221ae8058
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-02-23 00:12:16 -0800

    practice javascript

---
+++ prompt/app.js
@@ -0,0 +1,26 @@
+// prompht se hum user se input lete hain
+let num1 = prompt("First Value");
+let num2 = prompt("Second Value");
+
+// string ko number me convert kiya hai
+num1 = Number(num1);
+num2 = Number(num2);
+
+let sum = num1 + num2;
+let minus = num1 - num2;
+let multiply = num1 * num2;
+let divide = num1 / num2;
+
+console.log("Addition : " + sum);
+console.log("Minus : " + minus);
+console.log("Multiplication : " + multiply);
+console.log("Division : " + divide);
+
+// hum prompt ky input me value bi show kr sakte hain 
+// let num1 = prompt("First Value",'shahwaiz');
+
+// agar variable me kuch define nhi hoga tw undefine store hoga
+let shahwaiz;  //undefined
+//bad khali varibale ko value de sakte hain  
+shahwaiz = 50;
+console.log(shahwaiz);
---
+++ variables/app.js
@@ -0,0 +1,23 @@
+//   practice
+
+//variable
+//text is also called string
+
+let name = "syed shahwaiz";
+console.log(name);
+
+//number
+let sum = 6;
+console.log(sum);
+
+//const ko change nhi kr sakhte
+const pi = 3.142;
+console.log(pi);
+
+let a = prompt("value");
+console.log(a);
+
+let b = parseInt(prompt("first value"));
+let c = parseInt(prompt("second value"));
+let d = b + c;
+alert(d);
