3824ee810421a7bb8ff1f7480798bb8aef5a645e
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-03-20 08:19:26 -0700

    array folder add

---
+++ Array/app.js
@@ -0,0 +1,21 @@
+let colors = ["Red","Brown","Yellow","Gray","White"];
+
+
+//start se
+//colors.push("Black"); // push end me value add krta hai
+//colors.pop()  // pop end se value remove krta hai
+
+
+//End se
+//not recommended
+//colors.unshift("Black") // unshift start se element add krta hai
+//colors.shift() //shift start se element remove krta hai
+
+
+//console.log(colors.at(-1)); // prints last value prrint 1st method
+//console.log(colors[colors.length -1]); // last value print 2nd method
+
+//console.log(colors.length); // ye array ki length maloom krne ky liye use krte hain
+console.log(colors);
+
+//console.log(colors[3]);
