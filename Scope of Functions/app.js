0babfb2619326584b03a62bd132428d3c6a3b5ba
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-06-05 06:55:04 -0700

    scopes practice

--- Scope of Functions/app.js
+++ Scope of Functions/app.js
@@ -34,3 +34,52 @@ let x = 5;
 }
 x += 1;
 //console.log(x); // 7
+
+
+// chai our code series practice
+
+function one(){
+
+    const username = "Shahwaiz";
+    
+    function two(){
+        const website = "Youtube";
+        //console.log(username);
+    }
+
+    // console.log(website); // error website is not defined
+
+    two()
+
+}
+
+one();
+
+
+
+if (true) {
+    const username = "Qasim";
+    if (username === "Qasim") {
+        const website = "Youtube";
+       // console.log(`${username} ${website}`);
+    }
+    // console.log(website); // error
+}
+
+
+// ++++++++++++++++++++++++++++++++++interesting++++++++++++++++++++++++++++++++
+
+// console.log(addone(5));
+function addone(num){  // is function ko hum is function se pehly bi print karwa sakty hain  beacuse humne is function ko sirf decalre kiya hai
+   return num + 1;
+}
+
+
+// Hoisting
+
+console.log(addTwo(5)); // Error
+/* is function ko hum is function se pehly access nhi kar sakte because humne is funcyion ko sirf decalar nhi kiya 
+is function ko humne ak variable me store krdia */
+const addTwo = function(nums){  
+    return nums + 2;
+}
