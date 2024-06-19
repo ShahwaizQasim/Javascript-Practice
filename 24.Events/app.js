96270003b9404c53e5a7e6a4038a74fd9dd9b3bb
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-06-19 00:48:46 -0700

    events add

--- 24.Events/app.js
+++ 24.Events/app.js
@@ -1,3 +1,11 @@
+/* Events */
+// 1. click
+// 2. submit
+// 3.mousemove
+// 4.mouseout
+// 5.mouseover
+
+
 const body = document.querySelector("body");
 
 body.addEventListener("mousemove", (event) => {
