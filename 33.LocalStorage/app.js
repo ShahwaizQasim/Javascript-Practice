c37e8b7056cbd03faca8e9bd052f92c8cd4f4bc4
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-06-25 09:09:18 -0700

    localStorage Practice

--- 33.LocalStorage/app.js
+++ 33.LocalStorage/app.js
@@ -1,15 +1,31 @@
 
 /* localStorage me key bi string honi chahiye and value bi  */ 
 
-const key = prompt("Enter key you want to set");
-const value = prompt("Enter value you want to set")
+// const key = prompt("Enter key you want to set");
+// const value = prompt("Enter value you want to set");
 
-localStorage.setItem(key, value); // setItem localStorage me value ko set krta hai
+// localStorage.setItem(key, value); // setItem localStorage me value ko set krta hai
 
-const getElement = localStorage.getItem(key); // getItem key ki value batata hai      
+// const getElement = localStorage.getItem(key); // getItem key ki value batata hai
 // console.log(getElement); 
-console.log(`The value at ${key} is ${getElement}`);
+// console.log(`The value at ${key} is ${localStorage.getItem(key)}`); 
 
-if (key == "black" || key == "pink") {
-    localStorage.getItem(key).removeItem();
+// if (key == "color" || value == "red") {
+    // localStorage.removeItem(key); // removeItem localStorage se value ko remove krta hai
+// }   
+
+// if (key === "0") {
+    // localStorage.clear(); // clear puri localStorage ko empty kar deta hai
+// }
+
+
+
+const userMode = localStorage.getItem("mode");
+
+if (!userMode) {
+    const mode = prompt("Enter Your Mode Preference?")
+    localStorage.setItem("mode", mode)
+    localStorage.setItem("Name", "ali");
 }
+
+console.log(userMode);
