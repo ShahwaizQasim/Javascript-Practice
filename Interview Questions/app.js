d05fb2bf6fbe02c0a25ee03864a269ac8abd85db
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-25 02:05:48 -0800

    practce

--- Interview Questions/app.js
+++ Interview Questions/app.js
@@ -25,7 +25,7 @@ console.log(`a = ${a} and b ${b}`);
 
 
 // Question 2
-// write a function akk Array banao uss multiple values daalo or jab se vari value ussy print krwa do 
+// write a function akk Array banao uss me multiple string store krwao Task ye hai array me jo string sab se bara hai ussy print krwa do
 
 function findLargestValue(arr){
     let largestValue = "";
@@ -38,7 +38,34 @@ function findLargestValue(arr){
 }
 
 const arr = ["hello", "I","helloShahwaiz"]
-
-
 console.log(findLargestValue(arr));
+
+
+
+// Q3)
+//  ak Array me String store krwo phr unko reverse krwo without use in built-in function like reverse 
+const reversedArray = (arr) => {
+    let reversedArray = [];
+    for (let i = arr.length-1; i >= 0 ; i--) {
+        reversedArray.push(arr[i]);
+    }
+    return reversedArray;
+}
+
+const reverseArr = ["ahsan", "qasim", "junaid","talha"];
+console.log("ReversedArray", reversedArray(reverseArr));
+
+
+// const reversArray = ["Ali","Qasim","Junaid","Kashif","Hassan"]
+// for (let i = reversArray.length-1; i >= 0; i--) {
+//     console.log(reversArray[i]);
+// }
+
+// console.log(reversedArray(reversArray));
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+    
+// }
+
 
